9babc7c4be2a0e8e85f08a8a9ee021e67033d3a1
Ahmed Azagaz <138176569+ahmedazagaz@users.noreply.github.com>
2023-12-22 15:10:45 +0100

    Delete 0x04-python-more_data_structures directory

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    square = [list(row) for row in matrix]
-    for i in square:
-        for j in range(len(i)):
-            i[j] = i[j] ** 2
-    return square
--- 0x04-python-more_data_structures/2-uniq_add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def uniq_add(my_list=[]):
-    result = 0
-    list = []
-    for i in my_list:
-        if i not in list:
-            list.append(i)
-            result += i
-    return result
